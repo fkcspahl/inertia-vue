e7c771b67e271cc7c8b47057f5bdbd8966f174cc
Sascha Pahl <s.pahl@fkc-online.com>
2025-03-21 12:00:47 +0100

    Add dummy user

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -10,7 +10,12 @@ class AboutController extends Controller
 {
     //
     public function __invoke(Request $request) {
-        $user = User::find(1);
+        $user = User::firstOrNew(
+            ['id' => 1],
+            ['name' => 'John Doe', 'email' => 'j.doe@mail.com']
+        );
+
+        // Wenn kein User gefunden wurde, erstelle einen neuen
 
         return Inertia::render('About', [
             'user' => $user
